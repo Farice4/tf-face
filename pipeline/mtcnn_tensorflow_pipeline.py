049ea52910bc9456c912c26b9cf356353b51a177
Yuanbin.Chen <cybing4@gmail.com>
2019-10-09 15:14:36 +0800

    Add mtcnn tensorflow pipeline
    Signed-off-by: Yuanbin.Chen <cybing4@gmail.com>

--- pipeline/mtcnn_tensorflow_pipeline.py
+++ pipeline/mtcnn_tensorflow_pipeline.py
@@ -1,3 +1,4 @@
+
 import random
 import kfp.ds1 as ds1
 import kfp.onprem as onprem
@@ -5,7 +6,7 @@ from kubernetes.client.models import V1EnvVar
 
 
 platform = 'onprem'
-PYTHONPATH = "/dataset/face_classification/src"
+PYTHONPATH = "/dataset/tensorflow-MTCNN"
 
 is_aligned = False
 
@@ -22,8 +23,8 @@ def face_classification(train_steps='30',
                         public_ip='10.1.0.15'):
     """
     Pipeline with three stages:
-      1. prepare the face detection align dataset
-      1. train face classifier
+      1. prepare the mtcnn tensorflow align dataset
+      1. train mtcnn tensorflow
       2. deploy atf-serving instance to the cluster
       3. deploy a web-ui to interact with it
     """
@@ -31,7 +32,7 @@ def face_classification(train_steps='30',
         data_vop = ds1.VolumeOp(
             name = "prepare_data_vop",
             storage_class="rook-ceph-fs",
-            resource_name="classfication-input",
+            resource_name="mtcnn-input",
             modes=ds1.VOLUME_MODE_RWM,
             size="30Gi"
         )
@@ -40,7 +41,7 @@ def face_classification(train_steps='30',
         output_vop = ds1.VolumeOp(
             name="prepare_output_vop",
             storage_class="rook-ceph-fs",
-            resource_name="classfication-output",
+            resource_name="mtcnn-output",
             modes=ds1.VOLUME_MODE_RWM,
             size="30Gi"
         )
@@ -50,27 +51,39 @@ def face_classification(train_steps='30',
     # change code and dataset to pvc
     align_dataset = ds1.ContainerOp(
         name="aligin_dataset",
-        image="face-classifier-gpu",
-        command=["/bin/sh" "-c", "echo 'begin moving data';mv /root/face_classification %s/;echo 'moving is finished;"
+        image="mtcnn-tensorflow-gpu",
+        command=["/bin/sh" "-c", "echo 'begin moving data';mv /root/tensorflow-MTCNN %s/;echo 'moving is finished;"
                  % str(dataset_dir)]
     )
     align_dataset.after(output_vop)
 
     # train model
-    train_emotion = ds1.ContainerOp(
-        name='train_emotion',
-        image='face-classifier-gpu',
-        command=["/bin/sh", "-c", "cd /dataset/face_classification/src; python3 train_emotion_classifier.py"]
+    train_pnet = ds1.ContainerOp(
+        name='train_pnet',
+        image='mtcnn-tensorflow-gpu',
+        command=["/bin/sh", "-c", "cd /dataset/tensorflow-MTCNN/preprocess; python gen_12net_data.py;"
+                                  "python gen_landmark_aug.py 12; python gen_imglist_pnet.py;"
+                                  "python gen_tfrecords.py 12; python ../train/train.py 12"]
     ).add_resource_limit("nvidia.com/gpu", 1)
-    train_emotion.after(align_dataset)
+    train_pnet.after(align_dataset)
 
-    train_gender = ds1.ContainerOp(
-        name="train_gender",
-        image="face-classifier-gpu",
-        command=["/bin/sh", "-c", "cd /dataset/face_classification/src; python3 train_emotion_classifier.py"]
+    train_rnet = ds1.ContainerOp(
+        name="train_rnet",
+        image="mtcnn-tensorflow-gpu",
+        command=["/bin/sh", "-c", "cd /dataset/tensorflow-MTCNN/preprocess; python gen_hard_example.py 12;"
+                                  "python gen_landmark_aug.py 24; python gen_tfrecords.py 24;"
+                                  "python ../train/train.py 24"]
     ).add_resource_limit("nvidia.com/gpu", 1)
-    train_gender.after(train_emotion)
+    train_rnet.after(train_pnet)
 
+    train_onet = ds1.ContainerOp(
+        name="train_onet",
+        image="mtcnn-tensorflow-gpu",
+        command=["/bin/sh", "-c", "cd /dataset/tensorflow-MTCNN/preprocess; python gen_hard_example.py 24;"
+                                  "python gen_landmark_aug.py 48; python gen_tfrecords.py 48;"
+                                  "python ../train/train.py 48"]
+    ).add_resource_limit("nvidia.com/gpu", 1)
+    train_onet.after(train_rnet)
 
 
 if __name__ == '__main__':
